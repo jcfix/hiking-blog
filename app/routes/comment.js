d78e3ed9ba3da818462a463f223ba30aae9caa01
Jessica Fix and Ryan Brown <jcfix.pdx@gmail.com>
2016-03-31 10:05:27 -0700

    Transfer comment-detail.hbs to comment.hbs

--- app/routes/comment.js
+++ app/routes/comment.js
@@ -3,5 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('comment', params.comment_id);
+  },
+  actions: {
+    save3(params) {
+      var newComment = this.store.createRecord('comment', params);
+      newComment.save();
+      this.transtitionTo('comment');
+    },
   }
 });
